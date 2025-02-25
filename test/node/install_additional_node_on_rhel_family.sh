b2add65d7d0533b01e112e631b6fa6f9e23b340d
Gary Macindoe <gary@garymacindoe.co.uk>
2025-02-25 18:23:37 +0000

    Bump LTS node version to 22.

--- test/node/install_additional_node_on_rhel_family.sh
+++ test/node/install_additional_node_on_rhel_family.sh
@@ -5,11 +5,11 @@ set -e
 # Optional: Import test library
 source dev-container-features-test-lib
 
-# 'lts' is now some version of node 20...
-check "version_on_path"  node -v | grep 20
+# 'lts' is now some version of node 22...
+check "version_on_path"  node -v | grep 22
 check "pnpm" bash -c "pnpm -v | grep 6.16.0"
 
-check "v20_installed" ls -1 /usr/local/share/nvm/versions/node | grep 20
+check "v22_installed" ls -1 /usr/local/share/nvm/versions/node | grep 22
 check "v14_installed" ls -1 /usr/local/share/nvm/versions/node | grep 14.19.3
 check "v17_installed" ls -1 /usr/local/share/nvm/versions/node | grep 17.9.1
 
